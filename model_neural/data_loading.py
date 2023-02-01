71f87494f28bd4458845443429e923881136911f
Christian Singer <christian.singer@mail.schwarz>
2023-02-01 11:59:22 +0100

    - use tensors to store audio_samples. - One-hot encode labels. - Add collation function for use in dataloader. - Add unittest for collate_audio.

--- model_neural/data_loading.py
+++ model_neural/data_loading.py
@@ -1,12 +1,23 @@
+"""
+    Data loading utilities for the MNIST audio dataset.
+    See also:
+    https://pytorch.org/tutorials/intermediate/speech_command_classification_with_torchaudio_tutorial.html
+"""
+
 import os
-import librosa
+
 import pandas as pd
+import torch
+import torchaudio
 from torch.utils.data import Dataset
+from torch.nn.functional import one_hot
+
 
 class MNISTAudio(Dataset):
     def __init__(self, annotations_dir, audio_dir, split="TRAIN"):
         metadata = pd.read_csv(annotations_dir, sep="\t", header=0, index_col="Unnamed: 0")
-        self.audio_labels = metadata[metadata["split"] == split].label.values
+        audio_labels = torch.tensor(metadata[metadata["split"] == split].label.values)
+        self.audio_labels = one_hot(audio_labels, 10)
         self.audio_names = metadata[metadata["split"] == split].file.values
         self.audio_dir = audio_dir
 
@@ -15,6 +26,22 @@ class MNISTAudio(Dataset):
 
     def __getitem__(self, idx):
         audio_path = os.path.join(self.audio_dir, self.audio_names[idx])
-        audio, _ = librosa.load(audio_path)
+        audio, _ = torchaudio.load(audio_path)
         label = self.audio_labels[idx]
         return audio, label
+
+
+def collate_audio(batch):
+    """Collate a batch of audio samples and labels into a batch of tensors."""
+    tensors, targets = [], []
+
+    # Gather in lists, and encode labels as indices
+    for waveform, label in batch:
+        tensors += [waveform]
+        targets += [label]
+
+    # Group the list of tensors into a batched tensor
+    tensors = torch.nn.utils.rnn.pad_sequence(tensors, batch_first=True, padding_value=0.0)
+    targets = torch.stack(targets)
+
+    return tensors, targets
--- tests/unittests/model_neural/test_data_loading.py
+++ tests/unittests/model_neural/test_data_loading.py
@@ -1,11 +1,13 @@
 from pathlib import Path
 from unittest import TestCase
 
-import numpy as np
+import torch
 
-from model_neural.data_loading import MNISTAudio
+from model_neural.data_loading import MNISTAudio, collate_audio
 
 base_dir = Path(__file__).parent.parent.parent.parent
+
+
 class TestMNISTAudio(TestCase):
     """ Test the MNISTAudio class. """
 
@@ -20,6 +22,24 @@ class TestMNISTAudio(TestCase):
         audio, label = dataset[0]
         # train dataset has 2000 samples. See DataExploration.ipynb.
         self.assertTrue(len(dataset) == 2000)
-        self.assertTrue(isinstance(audio, np.ndarray))
-        self.assertTrue(audio.ndim == 1)
-        self.assertTrue(isinstance(label, np.int64))
+        self.assertTrue(isinstance(audio, torch.Tensor))
+        self.assertTrue(audio.shape[0] == 1)
+        self.assertTrue(isinstance(label, torch.Tensor))
+
+
+class TestCollation(TestCase):
+
+    def setUp(self):
+        self.tensor_1 = torch.tensor([1, 2, 3])
+        self.label_1 = torch.tensor([1, 0, 0, 0])
+        self.tensor_2 = torch.tensor([4, 5, 6, 7])
+        self.label_2 = torch.tensor([0, 1, 0, 0])
+        self.tensor_3 = torch.tensor([8, 9])
+        self.label_3 = torch.tensor([0, 0, 1, 0])
+    def test_collate_audio(self):
+        """Test that the pad_sequence function returns the correct shape."""
+        batch = [(self.tensor_1, self.label_1), (self.tensor_2, self.label_2), (self.tensor_3, self.label_3)]
+        padded_batch, targets = collate_audio(batch)
+        self.assertTrue(padded_batch.shape == (3, 4))
+        self.assertTrue(targets.shape == (3,4))
+
