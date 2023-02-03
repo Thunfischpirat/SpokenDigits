8947391118dca73c9d868a3b0bbb8c1072286fbe
Christian Singer <christian.singer@mail.schwarz>
2023-02-03 17:12:33 +0100

    - Add accuracy metric to classification_report

--- model_neural/classification_report.py
+++ model_neural/classification_report.py
@@ -23,7 +23,7 @@ loaders = dict([(split, DataLoader(MNISTAudio(annotations_dir=annotations_dir, a
                                               collate_fn=collate_audio,
                                               shuffle=True)) for split in ["TRAIN", "DEV", "TEST"]])
 
-def classification_report(*args):
+def classification_report(accuracy, *args):
     """ Creates a classification report for a model in form of a formatted string."""
     metrics = torch.vstack([*args]).t()
     report = "   ".join(f"{col}" for col in ["class", "precision", "recall", "f1-score"]) + "\n"
@@ -32,6 +32,7 @@ def classification_report(*args):
         for value in row:
             line += f"{value.item():.2f}       "
         report += f"{line}\n"
+    report += f"\naccuracy: {accuracy.item():.2f}\n"
     return report
 
 
@@ -39,11 +40,13 @@ confusion_matrix = tm.classification.MulticlassConfusionMatrix(num_classes=10)
 f1_score = tm.classification.MulticlassF1Score(num_classes=10, average="none")
 precision_metric = tm.classification.MulticlassPrecision(num_classes=10, average="none")
 recall_metric = tm.classification.MulticlassRecall(num_classes=10, average="none")
+accuracy_metric = tm.classification.MulticlassAccuracy(num_classes=10)
 
 confusion_matrix.to(device)
 f1_score.to(device)
 precision_metric.to(device)
 recall_metric.to(device)
+accuracy_metric.to(device)
 
 # https://discuss.pytorch.org/t/model-eval-vs-with-torch-no-grad/19615/2
 with torch.no_grad():
@@ -59,16 +62,19 @@ with torch.no_grad():
             f1_score(pred, target)
             precision_metric(pred, target)
             recall_metric(pred, target)
+            accuracy_metric(pred, target)
 
         cm = confusion_matrix.compute()
         f1 = f1_score.compute()
         precision = precision_metric.compute()
         recall = recall_metric.compute()
+        accuracy = accuracy_metric.compute()
 
         confusion_matrix.reset()
         f1_score.reset()
         precision_metric.reset()
         recall_metric.reset()
+        accuracy_metric.reset()
         print(f"Confusion matrix of {loader_name.lower()}-set:\n {cm}\n")
-        report = classification_report(precision, recall, f1)
+        report = classification_report(accuracy, precision, recall, f1)
         print(f"Classification report: {loader_name.lower()}-set:\n {report}")
