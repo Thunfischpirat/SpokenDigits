e9c80467ac1486e6e895de40d6b5a7aa41de3ebd
Christian Singer <christian.singer@mail.schwarz>
2023-03-12 11:33:50 +0100

    - Incorporate class PatchEmbedding into transformer_model class directly.

--- model_neural/transformer_model.py
+++ model_neural/transformer_model.py
@@ -24,16 +24,6 @@ class PositionalEncoding(nn.Module):
         x = x + self.P[:, : x.shape[1], :].to(x.device)
         return self.dropout(x)
 
-
-class PatchEmbedding(nn.Module):
-    def __init__(self, patch_size, stride, num_hidden):
-        super().__init__()
-        self.conv = nn.LazyConv2d(num_hidden, kernel_size=patch_size, stride=stride)
-
-    def forward(self, x):
-        return self.conv(x).flatten(2).transpose(1, 2)
-
-
 class ViTMLP(nn.Module):
     def __init__(self, mlp_num_hidden, mlp_num_outputs):
         super().__init__()
@@ -72,7 +62,7 @@ class transformer_model(nn.Module):
         num_classes=10,
     ):
         super().__init__()
-        self.patch_embedding = PatchEmbedding(patch_size, stride, num_hidden)
+        self.patch_embedding = nn.LazyConv2d(num_hidden, kernel_size=patch_size, stride=stride)
         self.cls_token = nn.Parameter(torch.zeros(1, 1, num_hidden))
         self.pos_embedding = PositionalEncoding(num_hidden, 0.1)
         self.dropout = nn.Dropout(0.1)
@@ -86,8 +76,8 @@ class transformer_model(nn.Module):
     def forward(self, x):
         # Add channel dimension.
         x = x.unsqueeze(1)
-        x = self.patch_embedding(x)
-        # Add cls token for each sample in the batch.
+        x = self.patch_embedding(x).flatten(2).transpose(1, 2)
+        # Using .expand adds cls token for each sample in the batch.
         cls_tokens = self.cls_token.expand(x.shape[0], -1, -1)
         x = torch.cat((cls_tokens, x), dim=1)
         x = x + self.pos_embedding(x)
@@ -102,9 +92,10 @@ if __name__ == "__main__":
 
     model = transformer_model()
 
-    model = train_model(model, n_epoch=100, lr=0.0001, to_mel=True)
+    trained_model = train_model(model, n_epoch=100, lr=0.0001, to_mel=True)
 
-    num_params = count_parameters(model)
+    # Since we are using LazyLinear, we need to call the model once to initialize the weights.
+    num_params = count_parameters(trained_model)
     print("Number of parameters: %s" % num_params)
 
-    torch.save(model.state_dict(), "models/transformer_model.pt")
+    torch.save(trained_model.state_dict(), "models/transformer_model.pt")
