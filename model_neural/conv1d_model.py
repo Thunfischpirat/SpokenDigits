9e6b236808090d7962921b2befd5407f31d83328
Christian Singer <christian.singer@mail.schwarz>
2023-03-12 11:32:03 +0100

    - Add convolutional model that can handle mel-spectrogram data.

--- model_neural/conv1d_model.py
+++ model_neural/conv1d_model.py
@@ -1,44 +1,65 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from utils.helpers import train_model, count_parameters
 
+
+class conv1d_block(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, pool_size=4):
+        super().__init__()
+        self.conv = nn.Conv1d(in_channels, out_channels, kernel_size=kernel_size, stride=stride)
+        self.bn = nn.BatchNorm1d(out_channels)
+        self.pool = nn.MaxPool1d(pool_size)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = F.relu(x)
+        x = self.pool(x)
+        return x
 
 
 class conv1d_model(nn.Module):
+    """Convolutional neural network for 1D data."""
+
     def __init__(
         self, n_input=1, n_channel=32, n_output=10, initial_kernel_size=60, initial_stride=8
     ):
         super().__init__()
-        self.conv1 = nn.Conv1d(
-            n_input, n_channel, kernel_size=initial_kernel_size, stride=initial_stride
-        )
-        self.bn1 = nn.BatchNorm1d(n_channel)
-        self.pool1 = nn.MaxPool1d(4)
-        self.conv2 = nn.Conv1d(n_channel, n_channel, kernel_size=3)
-        self.bn2 = nn.BatchNorm1d(n_channel)
-        self.pool2 = nn.MaxPool1d(4)
-        self.conv3 = nn.Conv1d(n_channel, 2 * n_channel, kernel_size=3)
-        self.bn3 = nn.BatchNorm1d(2 * n_channel)
-        self.pool3 = nn.MaxPool1d(4)
-        self.conv4 = nn.Conv1d(2 * n_channel, 2 * n_channel, kernel_size=3)
-        self.bn4 = nn.BatchNorm1d(2 * n_channel)
-        self.pool4 = nn.MaxPool1d(4)
+        self.conv_block1 = conv1d_block(n_input, n_channel, initial_kernel_size, initial_stride)
+        self.conv_block2 = conv1d_block(n_channel, n_channel)
+        self.conv_block3 = conv1d_block(n_channel, 2 * n_channel)
+        self.conv_block4 = conv1d_block(2 * n_channel, 2 * n_channel)
         self.fc1 = nn.Linear(2 * n_channel, n_output)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = F.relu(self.bn1(x))
-        x = self.pool1(x)
-        x = self.conv2(x)
-        x = F.relu(self.bn2(x))
-        x = self.pool2(x)
-        x = self.conv3(x)
-        x = F.relu(self.bn3(x))
-        x = self.pool3(x)
-        x = self.conv4(x)
-        x = F.relu(self.bn4(x))
-        x = self.pool4(x)
+        x = self.conv_block1(x)
+        x = self.conv_block2(x)
+        x = self.conv_block3(x)
+        x = self.conv_block4(x)
+        x = F.avg_pool1d(x, x.shape[-1])
+        x = x.permute(0, 2, 1)
+        x = self.fc1(x)
+        return F.log_softmax(x, dim=2)
+
+
+class conv1d_mel_model(nn.Module):
+    """Convolutional neural network for 1D data."""
+
+    def __init__(
+        self, n_input=39, n_channel=32, n_output=10, initial_kernel_size=16, initial_stride=3
+    ):
+        super().__init__()
+        self.conv_block1 = conv1d_block(
+            n_input, n_channel, initial_kernel_size, initial_stride, pool_size=2
+        )
+        self.conv_block2 = conv1d_block(n_channel, n_channel, pool_size=1)
+        self.conv_block3 = conv1d_block(n_channel, 2 * n_channel, pool_size=1)
+        self.fc1 = nn.Linear(2 * n_channel, n_output)
+
+    def forward(self, x):
+        x = self.conv_block1(x)
+        x = self.conv_block2(x)
+        x = self.conv_block3(x)
         x = F.avg_pool1d(x, x.shape[-1])
         x = x.permute(0, 2, 1)
         x = self.fc1(x)
@@ -48,18 +69,21 @@ class conv1d_model(nn.Module):
 if __name__ == "__main__":
     # Based on https://pytorch.org/tutorials/intermediate/speech_command_classification_with_torchaudio_tutorial.html
 
+    from utils.helpers import train_model, count_parameters
+
     to_mel = False
 
     if to_mel:
-        model = conv1d_model(n_input=39, initial_kernel_size=6, initial_stride=1)
+        model_name = "conv1d_mel_model"
+        model = conv1d_mel_model()
     else:
+        model_name = "conv1d_model"
         model = conv1d_model()
-
-    model = train_model(model, lr=0.01, to_mel=False)
 
     print(f"Number of parameters: {count_parameters(model)}")
 
-    torch.save(model.state_dict(), "models/conv1d_model.pt")
+    model = train_model(model, lr=0.001, to_mel=to_mel)
 
 
 
+    torch.save(model.state_dict(), f"models/{model_name}.pt")
--- tests/unittests/model_neural/test_conv1d_model.py
+++ tests/unittests/model_neural/test_conv1d_model.py
@@ -1,5 +1,5 @@
 import torch
-from model_neural.conv1d_model import conv1d_model
+from model_neural.conv1d_model import conv1d_model, conv1d_mel_model
 from unittest import TestCase
 
 class TestConv1dModel(TestCase):
@@ -7,5 +7,12 @@ class TestConv1dModel(TestCase):
         """Test that the conv1d_model returns the correct shape."""
         model = conv1d_model()
         x = torch.randn(64, 1, 4830)
+        y = model(x)
+        self.assertTrue(y.shape == (64, 1, 10))
+
+    def test_conv1d_mel_model(self):
+        """Test that the conv1d_model returns the correct shape."""
+        model = conv1d_mel_model()
+        x = torch.randn(64, 39, 80)
         y = model(x)
         self.assertTrue(y.shape == (64, 1, 10))
