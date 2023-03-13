d1e13b0d724e91ff01a0adcaa077904fd8dad855
Christian Singer <christian.singer@mail.schwarz>
2023-03-13 11:09:42 +0100

    - Adapt unit-tests to new names of neural network models.

--- tests/unittests/model_neural/test_conv1d_model.py
+++ tests/unittests/model_neural/test_conv1d_model.py
@@ -1,20 +1,20 @@
 from unittest import TestCase
 
 import torch
-from model_neural.conv1d_model import conv1d_mel_model, conv1d_model
+from model_neural.conv1d_model import Conv1dModel, Conv1dMelModel
 
 
 class TestConv1dModel(TestCase):
     def test_conv1d_model(self):
         """Test that the conv1d_model returns the correct shape."""
-        model = conv1d_model()
+        model = Conv1dModel()
         x = torch.randn(64, 1, 4830)
         y = model(x)
         self.assertTrue(y.shape == (64, 1, 10))
 
     def test_conv1d_mel_model(self):
         """Test that the conv1d_model returns the correct shape."""
-        model = conv1d_mel_model()
+        model = Conv1dMelModel()
         x = torch.randn(64, 39, 80)
         y = model(x)
         self.assertTrue(y.shape == (64, 1, 10))
