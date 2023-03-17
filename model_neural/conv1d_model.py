98037f8f532e2e6f4ecd0c44ad81528f9deca4db
Christian Singer <christian.singer@mail.schwarz>
2023-03-17 09:31:26 +0100

    - Add support for raw audio transforms from torchaudio.functional.
    - Make model structure customizable for training with contrastive loss.

--- model_neural/conv1d_model.py
+++ model_neural/conv1d_model.py
@@ -1,4 +1,5 @@
-import torch
+from typing import Union
+
 import torch.nn as nn
 import torch.nn.functional as F
 import torchaudio
@@ -35,7 +36,10 @@ class Conv1dModel(nn.Module):
         self.conv_block2 = conv1d_block(n_channel, n_channel)
         self.conv_block3 = conv1d_block(n_channel, 2 * n_channel)
         self.conv_block4 = conv1d_block(2 * n_channel, 2 * n_channel)
-        self.fc1 = nn.Linear(2 * n_channel, n_output)
+        if n_output is None:
+            self.fc1 = nn.Identity()
+        else:
+            self.fc1 = nn.Linear(2 * n_channel, n_output)
 
     def forward(self, x):
         x = self.conv_block1(x)
@@ -52,15 +56,32 @@ class Conv1dMelModel(nn.Module):
     """Convolutional neural network for 1D data."""
 
     def __init__(
-        self, n_input=39, n_channel=32, n_output=10, initial_kernel_size=16, initial_stride=3
+        self,
+        n_input: int = 39,
+        n_channel: int = 32,
+        n_output: Union[int, None] = 10,
+        initial_kernel_size: int = 16,
+        initial_stride: int = 3,
     ):
+        """
+        Args:
+            n_input: Number of input channels.
+            n_channel: Number of channels in the first convolutional layer.
+                       Subsequent layers will have 2 times the number of channels.
+            n_output: Number of output classes. Can be set to None to use the model for contrastive learning.
+            initial_kernel_size: Kernel size of the first convolutional layer.
+            initial_stride: Stride of the first convolutional layer.
+        """
         super().__init__()
         self.conv_block1 = conv1d_block(
             n_input, n_channel, initial_kernel_size, initial_stride, pool_size=2
         )
         self.conv_block2 = conv1d_block(n_channel, n_channel, pool_size=1)
         self.conv_block3 = conv1d_block(n_channel, 2 * n_channel, pool_size=1)
-        self.fc1 = nn.Linear(2 * n_channel, n_output)
+        if n_output is None:
+            self.fc1 = nn.Identity()
+        else:
+            self.fc1 = nn.Linear(2 * n_channel, n_output)
 
     def forward(self, x):
         x = self.conv_block1(x)
@@ -74,9 +95,7 @@ class Conv1dMelModel(nn.Module):
 
 if __name__ == "__main__":
     # Based on https://pytorch.org/tutorials/intermediate/speech_command_classification_with_torchaudio_tutorial.html
-    torch.manual_seed(32)
-    from utils.helpers import (count_parameters, optimize_hyperparams,
-                               train_model)
+    from utils.helpers import count_parameters, optimize_hyperparams, train_model
 
     to_mel = True
     optimize_hp = False
@@ -85,10 +104,11 @@ if __name__ == "__main__":
 
     if to_mel:
         model = Conv1dMelModel()
-        spec_transforms = [
-            torchaudio.transforms.FrequencyMasking(20),
-            torchaudio.transforms.TimeMasking(30),
+        audio_transforms = [
+            (torchaudio.functional.contrast, 0.5),
+            (torchaudio.functional.dither, 0.5),
         ]
+        spec_transforms = None
     else:
         # Expect error when training with speaker based datasets due to small input size for some samples.
         model = Conv1dModel()
@@ -101,9 +121,10 @@ if __name__ == "__main__":
             train_set,
             val_set,
             to_mel,
+            audio_transforms=audio_transforms,
             spec_transforms=spec_transforms,
             lr=0.0008,
-            weight_decay=0.001,
+            weight_decay=0.1,
             step_size=20,
             gamma=0.2,
         )
