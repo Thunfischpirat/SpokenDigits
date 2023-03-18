16cbd8713d7d6d8d29847a0f32763099531574ed
Christian Singer <christian.singer@mail.schwarz>
2023-03-18 15:53:08 +0100

    - Enable creation of t-SNE model embeddings for speaker based data subsets.

--- model_neural/utils/helpers.py
+++ model_neural/utils/helpers.py
@@ -6,7 +6,7 @@ from typing import Callable, List, Tuple, Union
 
 import torch
 import torch.nn.functional as F
-from model_neural.utils.data_loading import MNISTAudio, collate_audio
+from model_neural.utils.data_loading import MNISTAudio, collate_audio, collate_contrastive
 from torch import nn, optim
 from torch.utils.data import DataLoader
 
@@ -18,13 +18,34 @@ def count_parameters(model: nn.Module):
     """Count the number of trainable parameters in a model."""
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
+def info_nce_loss(embeddings):
+
+    temperature = 0.1
+
+    cos_sim = F.cosine_similarity(embeddings, embeddings.permute(1,0,2), dim=-1)
+
+    self_mask = torch.eye(cos_sim.shape[0], dtype=torch.bool, device=cos_sim.device)
+    cos_sim.masked_fill_(self_mask, -9e15)
+
+    pos_mask = self_mask.roll(shifts=1, dims=0)
+    cos_sim = cos_sim / temperature
+    nll = -cos_sim[pos_mask] + torch.logsumexp(cos_sim, dim=-1)
+    nll = nll.mean()
+
+    comb_sim = torch.cat([cos_sim[pos_mask][:, None],  # First position positive example
+                          cos_sim.masked_fill(pos_mask, -9e15)],
+                         dim=-1)
+    sim_argsort = comb_sim.argsort(dim=-1, descending=True).argmin(dim=-1)
+
+    return nll, sim_argsort
+
 
 def get_data_loaders(
     train_set: Union[str, List[str]],
     val_set: Union[str, List[str]],
-    to_mel: bool = False,
+    to_mel: bool = True,
     audio_transforms: List[Tuple[Callable, float]] = None,
-    spec_transforms: List[nn.Module] = None,
+    spec_transforms: nn.Module = None,
     batch_size: int = 64,
 ):
     """
@@ -35,7 +56,7 @@ def get_data_loaders(
         val_set: Name of the validation set to use.
         to_mel: Whether to convert the raw audio to a mel spectrogram first.
         audio_transforms: A list of raw audio transforms with execution probability.
-        spec_transforms: A list of spectrogram transforms.
+        spec_transforms: A sequence of spectrogram transforms.
         batch_size: The batch size to use.
     """
     train_set = MNISTAudio(
@@ -46,15 +67,24 @@ def get_data_loaders(
         audio_transforms=audio_transforms,
         spec_transforms=spec_transforms,
     )
+
+    # Check if training is unsupervised.
+    if spec_transforms.__class__.__name__ == "ContrastiveTransformations":
+        collate_fn = collate_contrastive
+        spec_transforms_val = spec_transforms
+    else:
+        collate_fn = collate_audio
+        spec_transforms_val = None
+
     train_loader = DataLoader(
-        train_set, batch_size=batch_size, collate_fn=collate_audio, shuffle=True
+        train_set, batch_size=batch_size, collate_fn=collate_fn, shuffle=True
     )
 
     validation_set = MNISTAudio(
-        annotations_dir=annotations_dir, audio_dir=base_dir, split=val_set, to_mel=to_mel
+        annotations_dir=annotations_dir, audio_dir=base_dir, split=val_set, to_mel=to_mel, spec_transforms=spec_transforms_val
     )
     validation_loader = DataLoader(
-        validation_set, batch_size=batch_size, collate_fn=collate_audio, shuffle=True
+        validation_set, batch_size=batch_size, collate_fn=collate_fn, shuffle=True
     )
 
     return train_loader, validation_loader
@@ -66,7 +96,7 @@ def train_model(
     val_set: Union[str, List[str]] = "DEV",
     to_mel: bool = False,
     audio_transforms: List[Tuple[Callable, float]] = None,
-    spec_transforms: List[nn.Module] = None,
+    spec_transforms: nn.Module = None,
     lr: float = 0.01,
     weight_decay: float = 0.01,
     step_size: int = 20,
@@ -84,7 +114,7 @@ def train_model(
         val_set: Name of the validation set to use.
         to_mel: Whether to convert the raw audio to a mel spectrogram first.
         audio_transforms: A list of raw audio transforms with execution probability.
-        spec_transforms: A list of spectrogram transforms.
+        spec_transforms: A sequence of spectrogram transforms.
         lr: The learning rate to use.
         weight_decay: The weight decay to use.
         step_size: The step size to use for the learning rate scheduler.
@@ -128,7 +158,6 @@ def train_model(
             target = target.to(device)
 
             output = model(data)
-            # output_sm = F.log_softmax(output, dim=2)
 
             loss = loss_func(output.squeeze(), target)
 
@@ -201,7 +230,7 @@ def optimize_hyperparams(
     val_set: Union[str, List[str]],
     to_mel: bool,
     audio_transforms: List[Tuple[Callable, float]],
-    spec_transforms: List[nn.Module],
+    spec_transforms: nn.Module,
     learning_rates: List[float],
     weight_decays: List[float],
     step_sizes: List[int],
@@ -216,7 +245,7 @@ def optimize_hyperparams(
         val_set: Name of the validation set to use.
         to_mel: Whether to convert the raw audio to a mel spectrogram first.
         audio_transforms: A list of raw audio transforms with execution probability.
-        spec_transforms: A list of spectrogram transforms.
+        spec_transforms: A sequence of spectrogram transforms.
         learning_rates: List of learning rates to try.
         weight_decays: List of weight decays to try.
         step_sizes: List of step sizes to try.
@@ -294,3 +323,112 @@ def optimize_hyperparams(
         file.write(f"Best parameters: {best_params}. Best loss: {best_loss}.\n")
 
     return best_model, best_params
+
+def contrastive_train(
+    model: nn.Module,
+    train_set: Union[str, List[str]] = "TRAIN",
+    val_set: Union[str, List[str]] = "DEV",
+    spec_transforms: nn.Module = None,
+    lr: float = 0.01,
+    weight_decay: float = 0.01,
+    batch_size: int = 32,
+    patience: int = 20,
+):
+    torch.manual_seed(32)
+    train_loader, validation_loader = get_data_loaders(
+        train_set=train_set,
+        val_set=val_set,
+        batch_size=batch_size,
+        spec_transforms=spec_transforms,
+    )
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Using: '{device}' as device for training.")
+
+    model.to(device)
+    model_name = model.__class__.__name__ + "_contrastive"
+
+    optimizer = optim.AdamW(model.parameters(),
+                            lr=lr,
+                            weight_decay=weight_decay)
+    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer,
+                                                        T_max=500,
+                                                        eta_min=lr / 50)
+
+    loss_val = None
+
+    # Used for early stopping, if enabled.
+    best_loss_val = float("inf")
+
+    n_epochs = 1000
+    for epoch in range(n_epochs):
+        # -------------------- TRAINING --------------------
+        model.train()
+        losses_train = []
+        for batch_idx, (data, _) in enumerate(train_loader):
+            data = data.to(device)
+
+            output = model(data)
+
+            loss, _ = info_nce_loss(output)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            losses_train.append(loss.item())
+
+        # -------------------- VALIDATION --------------------
+        model.eval()
+        losses_val = []
+        val_top1 =[]
+        val_top5 =[]
+        with torch.no_grad():
+            for data, _ in validation_loader:
+                data = data.to(device)
+                try:
+                    output = model(data)
+
+                    loss, sim_argsort = info_nce_loss(output)
+                    losses_val.append(loss.item())
+                    val_top1.append((sim_argsort == 0).float().mean())
+                    val_top5.append((sim_argsort < 5).float().mean())
+                except RuntimeError:
+                    print("Skipping batch due to small sample length.")
+
+        # Update the learning rate of the optimizer
+        scheduler.step()
+
+        if epoch % 5 == 0:
+            loss_train = sum(losses_train) / len(losses_train)
+            loss_val = sum(losses_val) / len(losses_val)
+            loss_top1 = 100 * (sum(val_top1) / len(val_top1))
+            loss_top5 = 100 * sum(val_top5) / len(val_top5)
+
+            print(
+                f"Epoch: {epoch} Train-Loss: {loss_train:.4f} "
+                f"Val-Loss: {loss_val:.4f} "
+                f"Val-Top1: {loss_top1:.2f} "
+                f"Val-Top5: {loss_top5:.3f}"
+            )
+
+        if epoch >= 5:
+            loss_val = sum(losses_val) / len(losses_val)
+            if loss_val < best_loss_val:
+                print(
+                    f"Improved validation loss to {loss_val:.4f} in epoch {epoch}."
+                    f" Saving model to disk!"
+                )
+                best_loss_val = loss_val
+                torch.save(
+                    model.state_dict(),
+                    f"models/{model_name}.pt",
+                )
+                patience = 20
+            elif patience == 0:
+                print(f"Validation loss didnt improve further. Stopping training in epoch {epoch}!")
+                return model, best_loss_val
+
+            patience -= 1
+
+    return model, loss_val
